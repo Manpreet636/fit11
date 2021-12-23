55580c00c9d9d58ecc67773058fec2fe5a3f0fec
Manpreet636 <75639402+Manpreet636@users.noreply.github.com>
2021-12-23 14:18:04 -0500

    Initial Commit

---
+++ fit11/fit11App.swift
@@ -0,0 +1,17 @@
+//
+//  fit11App.swift
+//  fit11
+//
+//  Created by Navtej Singh on 2021-12-23.
+//
+
+import SwiftUI
+
+@main
+struct fit11App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
